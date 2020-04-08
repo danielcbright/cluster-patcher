31788272c0757d68cf2f4ae12e6bb1be934955b8
Daniel Bright <danielcbright@gmail.com>
2020-04-08 15:52:57 -0400

    initial commit
    Signed-off-by: Daniel Bright <danielcbright@gmail.com>

---
+++ plan.sh
@@ -0,0 +1,13 @@
+pkg_name=cluster-patcher
+pkg_origin=dbright
+pkg_version="1.0.0"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("Apache-2.0")
+pkg_deps=(core/jq-static core/iptables)
+
+do_build() {
+  return 0
+}
+do_install() {
+  return 0
+}
