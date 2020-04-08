356cb2e0ae0660fd1c6f3678c0a7f5b8bb8b2266
Daniel Bright <danielcbright@gmail.com>
2020-04-08 15:59:26 -0400

    initial commit
    Signed-off-by: Daniel Bright <danielcbright@gmail.com>

---
+++ config.sh
@@ -0,0 +1,12 @@
+function bump_version () {
+    curr_number=`grep number ./config.toml | cut -d "=" -f2`
+    new_version=`echo $(date +%s)`
+    sudo sed -i "s/number\s*=\s*$curr_number/number = $new_version/g" ./config.toml
+    echo $new_version
+}
+function config_apply () {
+    apply_version=$(bump_version)
+    hab config apply cluster-patcher.default $apply_version ./config.toml
+}
+
+config_apply
